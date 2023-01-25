0243e6b9a24093b1ad2ffb2607310f96c10ba077
Pablo Dawson <74978011+pablodawson@users.noreply.github.com>
2023-01-25 15:04:30 -0300

    trigger rebuild

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from transformers import OneFormerProcessor, OneFormerForUniversalSegmentation
 from PIL import Image
 import numpy as np
 import time
+
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
