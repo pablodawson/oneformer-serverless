1ab564b6fbbfda46ba7cf3d89d4cea87c8529673
pablo <titodawson@gmail.com>
2023-01-25 10:43:41 -0300

    run on gpu

--- app.py
+++ app.py
@@ -5,25 +5,29 @@ from io import BytesIO
 from transformers import OneFormerProcessor, OneFormerForUniversalSegmentation
 from PIL import Image
 import numpy as np
+import time
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
     global processor
+    global device
+    
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
     models = ["shi-labs/oneformer_ade20k_swin_tiny", "shi-labs/oneformer_ade20k_dinat_large"]
-
+    timestart = time.time()
     processor = OneFormerProcessor.from_pretrained(models[1])
-    model = OneFormerForUniversalSegmentation.from_pretrained(models[1])
+    model = OneFormerForUniversalSegmentation.from_pretrained(models[1]).to(device)
+    print("Model loaded in: ", time.time() - timestart)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict, img_bytes) -> dict:
     global model
-
     # Parse out your arguments
     # Get file from request 
-
+    timestart = time.time()
     task = model_inputs.get("task", "semantic")
     #model = model_inputs.get("model", "shi-labs/oneformer_ade20k_swin_tiny")
     input_img = Image.open(BytesIO(img_bytes))
@@ -36,22 +40,22 @@ def inference(model_inputs:dict, img_bytes) -> dict:
         return {'message': "No task provided"}
     
     # Run the model
-    with autocast("cuda"):
-        inputs = processor(input_img, ["semantic"], return_tensors="pt")
-        with torch.no_grad():
-            outputs = model(**inputs)
-        
-        class_queries_logits = outputs.class_queries_logits
-        masks_queries_logits = outputs.masks_queries_logits
-
-        predicted_semantic_map = processor.post_process_semantic_segmentation(
-        outputs, target_sizes=[input_img.size[::-1]])[0]
-        predicted_semantic_map_np = predicted_semantic_map.cpu().numpy().astype(np.uint8)
-        image = Image.fromarray(predicted_semantic_map_np)
+    inputs = processor(input_img, ["semantic"], return_tensors="pt").to(device)
+    with torch.no_grad():
+        outputs = model(**inputs)
     
+    class_queries_logits = outputs.class_queries_logits
+    masks_queries_logits = outputs.masks_queries_logits
+
+    predicted_semantic_map = processor.post_process_semantic_segmentation(
+    outputs, target_sizes=[input_img.size[::-1]])[0]
+    predicted_semantic_map_np = predicted_semantic_map.cpu().numpy().astype(np.uint8)
+    image = Image.fromarray(predicted_semantic_map_np)
+
     buffered = BytesIO()
     image.save(buffered,format="JPEG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
+    print("Inference time: ", time.time() - timestart)
     # Return the results as a dictionary
     return {'image_base64': image_base64}
