f54e0f5abec47824f696f965acdd8f146ce6aba1
pablo <titodawson@gmail.com>
2023-04-04 15:13:02 -0400

    actual

--- utils.py
+++ utils.py
@@ -158,66 +158,70 @@ def get_angle(image, method=1, angle='radians'):
         else:
             return angleX, -angleY
 
-<<<<<<< HEAD
-def colorize(value, vmin=None, vmax=None, cmap='magma_r', invalid_val=-99, invalid_mask=None, background_color=(128, 128, 128, 255), gamma_corrected=False, value_transform=None):
-    """Converts a depth map to a color image.
-    Args:
-        value (torch.Tensor, numpy.ndarry): Input depth map. Shape: (H, W) or (1, H, W) or (1, 1, H, W). All singular dimensions are squeezed
-        vmin (float, optional): vmin-valued entries are mapped to start color of cmap. If None, value.min() is used. Defaults to None.
-        vmax (float, optional):  vmax-valued entries are mapped to end color of cmap. If None, value.max() is used. Defaults to None.
-        cmap (str, optional): matplotlib colormap to use. Defaults to 'magma_r'.
-        invalid_val (int, optional): Specifies value of invalid pixels that should be colored as 'background_color'. Defaults to -99.
-        invalid_mask (numpy.ndarray, optional): Boolean mask for invalid regions. Defaults to None.
-        background_color (tuple[int], optional): 4-tuple RGB color to give to invalid pixels. Defaults to (128, 128, 128, 255).
-        gamma_corrected (bool, optional): Apply gamma correction to colored image. Defaults to False.
-        value_transform (Callable, optional): Apply transform function to valid pixels before coloring. Defaults to None.
-    Returns:
-        numpy.ndarray, dtype - uint8: Colored depth map. Shape: (H, W, 4)
-    """
-    if isinstance(value, torch.Tensor):
-        value = value.detach().cpu().numpy()
+def draw_lines(lines, image):
+    # Draw the detected lines on a copy of the original image
+    copy = np.copy(image)
+    for i in range(len(lines)):
+        for x1,y1,x2,y2 in lines[i]:
+            cv2.line(copy, (x1,y1), (x2,y2), (0,255,0),2)
+    return copy
 
-    value = value.squeeze()
-    if invalid_mask is None:
-        invalid_mask = value == invalid_val
-    mask = np.logical_not(invalid_mask)
+def get_wall_instances(image, wall_segmentation, canny_tres_1=50, canny_tres_2=100, hough_angle_tres=3, hough_tres=120,debug=False):
 
-    # normalize
-    vmin = np.percentile(value[mask],2) if vmin is None else vmin
-    vmax = np.percentile(value[mask],85) if vmax is None else vmax
-    if vmin != vmax:
-        value = (value - vmin) / (vmax - vmin)  # vmin..vmax
-    else:
-        # Avoid 0-division
-        value = value * 0.
+    if (image.shape != wall_segmentation.shape):
+        wall_segmentation= cv2.resize(wall_segmentation, (image.shape[1], image.shape[0]))
+        wall_segmentation = wall_segmentation[:,:,:3]
+    
+    wall_only = np.zeros_like(image)
+    wall_only[wall_segmentation == 255] = image[wall_segmentation == 255]
 
-    # squeeze last dim if it exists
-    # grey out the invalid values
+    wall_only = cv2.cvtColor(wall_only, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(wall_only, canny_tres_1, canny_tres_2)
 
-    value[invalid_mask] = np.nan
-    cmapper = matplotlib.cm.get_cmap(cmap)
-    if value_transform:
-        value = value_transform(value)
-        # value = value / value.max()
-    value = cmapper(value, bytes=True)  # (nxmx4)
+    lines = cv2.HoughLinesP(edges, hough_angle_tres, np.pi/2, hough_tres, minLineLength=20, maxLineGap=0)
 
-    # img = value[:, :, :]
-    img = value[...]
-    img[invalid_mask] = background_color
+    if (debug):
+        cv2.imwrite('lines.png', draw_lines(lines, image))
+        cv2.imwrite('edges.png', edges)
+    
+    limits = []
 
-    #     return img.transpose((2, 0, 1))
-    if gamma_corrected:
-        # gamma correction
-        img = img / 255
-        img = np.power(img, 2.2)
-        img = img * 255
-        img = img.astype(np.uint8)
-    return img
+    for line in lines:
+        x1, x2 = line[0][0], line[0][2]
+        y1, y2 = line[0][1], line[0][3]
+        if (x1 == x2 and np.abs(y2-y1) > 20):
+            limits.append(x1)
+
+    #sort lines by x coordinate
+    
+    limits.sort()
+    for i in range(len(limits)-3):
+        if (i!=0):
+            if (limits[i] - limits[i-1] < 20):
+                limits.pop(i)
+
+    limits.insert(0,0)
+    limits.append(image.shape[1])
+
+    segmentations = []
+
+    
+    for i in range(len(limits) - 1):
+        start = limits[i]
+        end = limits[i+1]
+
+        seg = np.zeros_like(wall_segmentation)
+        
+        seg[:, start:end] = wall_segmentation[:, start:end]
+        
+        if (debug):
+            cv2.imwrite('seg_output_'+str(i)+'.png', seg)
+        
+        segmentations.append(seg)
+    
+    return segmentations
 
 if __name__ == "__main__":
     img = cv2.imread('input/bedroom.png')
     wallseg = cv2.imread('seg_output.png')
-=======
-if __name__ == "__main__":
-    pass
->>>>>>> parent of 9376fd3 (opciones)
+    get_wall_instances(img, wallseg, debug=True)
